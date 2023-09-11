22076365ee0a6c406fa391e12fd2057f0e861d9a
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-09-11 11:32:40 +0530

    | VP | 11/09/23 | Add Helper...

---
+++ lib/views/screens/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(
+          16,
+        ),
+        child: Center(),
+      ),
+    );
+  }
+}
