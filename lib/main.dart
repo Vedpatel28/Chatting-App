e7aa50ccf0c6a1254327769d97a158a521a3ff35
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-09-11 11:31:36 +0530

    | VP | 11/9/23 | add Login...

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:chat_app_firebase/views/screens/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
   runApp(const MyApp());
 }
 
@@ -15,9 +20,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-
+      getPages: [
+        GetPage(
+          name: "/",
+          page: () => LoginPage(),
+        ),
+      ],
     );
   }
 }
