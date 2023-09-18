6bbb036f6b82d5f0a1ada79473e3d0dc363a7e58
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-09-18 11:13:42 +0530

    | VP | 17/09/23| Starting FireBase ...

--- lib/helper/fire_store_helper.dart
+++ lib/helper/fire_store_helper.dart
@@ -9,36 +9,36 @@ class FireStoreHelper {
 
   FirebaseFirestore db = FirebaseFirestore.instance;
 
-  addStudent({required FireStoreModal fireStoreModal}) {
-    final user = <String, dynamic>{
-      "age": fireStoreModal.age,
-      "id": fireStoreModal.id,
-      "name": fireStoreModal.name,
-    };
-    db.collection("student").add(user).then(
-          (DocumentReference doc) => log(
-            'DocumentSnapshot added with ID: ${doc.id}',
-          ),
-        );
-  }
-
-  addData() {
-    final user = <String, dynamic>{
-      "age": 19,
-      "id": 102,
-      "name": "ved",
-    };
-    db.collection("student").add(user).then(
-          (DocumentReference doc) =>
-              log('DocumentSnapshot added with ID: ${doc.id}'),
-        );
-  }
-
-  readData() async {
-    return await db.collection("users").get().then((event) {
-      for (var doc in event.docs) {
-        log("${doc.id} => ${doc.data()}");
-      }
-    });
-  }
+  // addStudent({required FireStoreModal fireStoreModal}) {
+  //   final user = <String, dynamic>{
+  //     "age": fireStoreModal.age,
+  //     "id": fireStoreModal.id,
+  //     "name": fireStoreModal.name,
+  //   };
+  //   db.collection("student").add(user).then(
+  //         (DocumentReference doc) => log(
+  //           'DocumentSnapshot added with ID: ${doc.id}',
+  //         ),
+  //       );
+  // }
+  //
+  // addData() {
+  //   final user = <String, dynamic>{
+  //     "age": 19,
+  //     "id": 102,
+  //     "name": "ved",
+  //   };
+  //   db.collection("student").add(user).then(
+  //         (DocumentReference doc) =>
+  //             log('DocumentSnapshot added with ID: ${doc.id}'),
+  //       );
+  // }
+  //
+  // readData() async {
+  //   return await db.collection("users").get().then((event) {
+  //     for (var doc in event.docs) {
+  //       log("${doc.id} => ${doc.data()}");
+  //     }
+  //   });
+  // }
 }
