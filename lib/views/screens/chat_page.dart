2bb65b1a2dc4029f4acc84644a78fe712fe90dfb
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-09-20 17:42:03 +0530

    | VP | 20/09/23 | Chat Page Finding Error...

--- lib/views/screens/chat_page.dart
+++ lib/views/screens/chat_page.dart
@@ -1,28 +1,49 @@
+import 'dart:developer';
+
 import 'package:chat_app_firebase/helper/fire_store_helper.dart';
+import 'package:chat_app_firebase/modal/get_user_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ChatPage extends StatelessWidget {
-  ChatPage({super.key});
+  const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List userData = Get.arguments;
+    List<GetUserModal> userData = Get.arguments;
     return Scaffold(
+      appBar: AppBar(
+        title: StreamBuilder(
+          stream: FireStoreHelper.fireStoreHelper.userStream(
+            recievedId: userData[0].id,
+          ),
+          builder: (context, snapshot) {
+             Map data = snapshot.data as Map;
+             log("Data: $data \n Id: ${userData[0].name}");
+
+            if (snapshot.hasData) {
+              return Text("${data['sent']}");
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             StreamBuilder(
               stream: FireStoreHelper.fireStoreHelper.userStream(
-                sentId: userData[0],
-                recievedId: userData[0],
+                recievedId: userData[0].id,
               ),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Text("${userData[0]}");
+                  return Text("${userData[0].id}");
                 } else {
-                  return Center(
+                  return const Center(
                     child: CircularProgressIndicator(),
                   );
                 }
