fd947c61805fcc07d74b4613856617c3646988b8
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-09-26 14:18:16 +0530

    | VP | 26/09/23 | Notifications Add & Find Error...

---
+++ lib/helper/notifications_helper.dart
@@ -0,0 +1,48 @@
+import 'dart:developer';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationsHelper {
+  NotificationsHelper._();
+
+  static final NotificationsHelper notificationsHelper =
+      NotificationsHelper._();
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  simpleNotifications({
+    required int id,
+    required String title,
+    required String subTitle,
+  }) {
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      "$id",
+      "Chat App",
+      importance: Importance.high,
+      priority: Priority.high,
+    );
+
+    DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
+      subtitle: subTitle,
+    );
+
+    flutterLocalNotificationsPlugin
+        .show(
+      id,
+      title,
+      subTitle,
+      NotificationDetails(
+        android: androidNotificationDetails,
+        iOS: darwinNotificationDetails,
+      ),
+    )
+        .then((value) {
+      log("Notifications Showed");
+    }).catchError((error) {
+      log("Error : $error");
+    });
+  }
+}
